af0f803a5170f78f100ad7c7886e4585d94132c5
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-05-20 17:28:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ import streamlit as st
 st.write('test')
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
+st.write("例:", st.secrets["AZURE_OPENAI_ENDPOINT"])
+st.write("例:", st.secrets["AZURE_OPENAI_API_KEY"])
+st.write("例:", st.secrets["SEARCH_ENDPOINT"])
+st.write("例:", st.secrets["SEARCH_API_KEY"])
