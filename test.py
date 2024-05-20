8fee18adaaa4bbfd23efed69d975947dc6e6e291
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-05-20 17:38:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,3 @@ from openai import AzureOpenAI
 st.write('test')
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
-
-# Azure OpenAI クライアントの作成
-client = AzureOpenAI(
-    azure_endpoint=st.secrets["AZURE_OPENAI_ENDPOINT"],
-    api_key=st.secrets["AZURE_OPENAI_KEY"],
-    api_version="2024-02-15-preview"
-)
-
-
