1b47627b6b90927508ff90291d95c81557079f68
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-05-20 17:43:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 # streamlit_app.py
 import streamlit as st
 import os
-# from openai import AzureOpenAI
+from openai import AzureOpenAI
 
 st.write('test')
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
+
+# Azure OpenAI クライアントの作成
+client = AzureOpenAI(
+    azure_endpoint=st.secrets["AZURE_OPENAI_ENDPOINT"],
+    api_key=st.secrets["AZURE_OPENAI_KEY"],
+    api_version="2024-02-15-preview"
+)
