af67a489a7285d167e69cae3eb0967845ce9468b
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-06-13 10:01:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,12 @@ import os
 
 
 st.write('test')
-st.write(system_msg)
+
 system_msg = """If the user is asked about a product, please include all information about each product.
                 The user is an excellent salesman.
                 Please advise according to the user's request.
                 The user talks in a friendly tone, but somewhat difficult to understand.
                 Be sure to conduct the conversation and evaluation in Japanese."""
+
+st.write(system_msg)
+
