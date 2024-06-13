cfd9f3ddb362e024806f679f052f7a48d8b436aa
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-06-13 10:48:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,16 +7,7 @@ from login import check_password
 st.write('test')
 
 system_msg = """
-# フラグが1のレコード数
-flag_1_count = results_df['120フラグ'].sum()
 
-# 全レコード数
-total_records = len(results_df)
-
-# フラグが1の割合の計算
-flag_1_ratio = (flag_1_count / total_records) * 100
-
-print(f"「120フラグ」が1の割合: {flag_1_ratio:.2f}%")
 """
 
 st.write(system_msg)
