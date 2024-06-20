0d87531ba1884c2c5b5564e2ecef72ea2b8c9f12
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-06-20 22:23:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ from login import check_password
 
 # """
 
-st.write('result.columns = ['外気温'] + ['エアコン不使用者カウント' if col == 'エアコン不使用者' else 'エアコン利用者カウント' for col in result.columns[1:]]')
+st.write('')
 
