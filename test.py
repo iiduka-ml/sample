732c0387f7ee6b1ab66868f27147ae778f23b66b
iiduka-ml <160207439+iiduka-ml@users.noreply.github.com>
2024-06-20 16:17:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import os
 
 from login import check_password
 
-st.write('　')
+st.write('print(f"ID: {row['車両固有のID']}, 開始時間: {row['データを取得した時間']}, 次の時間: {row['next_time']}, 使用時間: {usage_time}")
+        return usage_time')
 # system_msg = 
 # """
 
